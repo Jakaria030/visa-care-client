a242e4e70264a20c7f768395a1e91b1565137fea
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-08 00:21:28 +0600

    Statistic section added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Banner from "../components/Banner";
 import LatestVisas from "../components/LatestVisas";
 import OurOfferings from "../components/Ourofferings";
+import Statistics from "../components/Statistics";
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
             <LatestVisas></LatestVisas>
+            <Statistics></Statistics>
             <OurOfferings></OurOfferings>
         </div>
     );
