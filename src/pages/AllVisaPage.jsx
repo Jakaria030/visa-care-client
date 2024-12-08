f458b8f4520cfbedf2ce7aa0c6f2b058fa4ec757
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-08 11:39:51 +0600

    Added search box in my vis application page

--- src/pages/AllVisaPage.jsx
+++ src/pages/AllVisaPage.jsx
@@ -12,7 +12,7 @@ const AllVisaPage = () => {
     const handleSelectedVisaType = (e) => {
         let selectedValue = e.target.textContent;
 
-        if(selectedValue === 'All Visa'){
+        if (selectedValue === 'All Visa') {
             selectedValue = "";
         }
         fetch(`${root_dir}/visas?filterByVisaType=${selectedValue}`)
@@ -35,14 +35,16 @@ const AllVisaPage = () => {
 
     return (
         <section className="max-w-8xl mx-auto px-5">
-            <div className="dropdown mt-10">
-                <div tabIndex={0} role="button" className="px-4 py-2 rounded-md bg-[#003366] font-inter text-white">Filter By Visa Type</div>
-                <ul tabIndex={0} className="dropdown-content menu rounded-md bg-base-100 z-[1] w-52 p-2 shadow">
-                    <li><a onClick={handleSelectedVisaType}>All Visa</a></li>
-                    <li><a onClick={handleSelectedVisaType}>Tourist Visa</a></li>
-                    <li><a onClick={handleSelectedVisaType}>Student Visa</a></li>
-                    <li><a onClick={handleSelectedVisaType}>Official Visa</a></li>
-                </ul>
+            <div className="flex items-center justify-center mt-10">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="px-4 py-2 rounded-md bg-[#003366] font-inter w-52 text-center text-white">Filter By Visa Type</div>
+                    <ul tabIndex={0} className="dropdown-content menu rounded-md bg-base-100 z-[1] w-52 p-2 shadow">
+                        <li><a onClick={handleSelectedVisaType}>All Visa</a></li>
+                        <li><a onClick={handleSelectedVisaType}>Tourist Visa</a></li>
+                        <li><a onClick={handleSelectedVisaType}>Student Visa</a></li>
+                        <li><a onClick={handleSelectedVisaType}>Official Visa</a></li>
+                    </ul>
+                </div>
             </div>
             {loading ? (
                 <LoadingPage />
--- src/pages/MyVisaApplicationPage.jsx
+++ src/pages/MyVisaApplicationPage.jsx
@@ -4,21 +4,22 @@ import { AuthContext } from "../provider/AuthProvider";
 import NoDataFound from "../components/NoDataFound";
 import Swal from "sweetalert2";
 import LoadingPage from "./LoadingPage";
+import { IoIosSearch } from "react-icons/io";
 
 const MyVisaApplicationPage = () => {
     const [myAppliedVisas, setMyAppliedVisa] = useState([]);
     // console.log(myAppliedVisas);
-    const {user, root_dir} = useContext(AuthContext);
+    const { user, root_dir } = useContext(AuthContext);
     const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetch(`${root_dir}/applications/${user.email}`)
-        .then(res => res.json())
-        .then(data => {
-            setMyAppliedVisa(data);
-            setLoading(false);
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setMyAppliedVisa(data);
+                setLoading(false);
+            })
+    }, [])
 
     // handle cancel visa
     const handleCancelVisa = (id) => {
@@ -31,46 +32,74 @@ const MyVisaApplicationPage = () => {
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
                 fetch(`${root_dir}/applications/${id}`, {
                     method: 'DELETE',
                 })
-                .then(res => res.json())
-                .then(data => {
-                    // console.log(data);
-                    if(data.deletedCount > 0){
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Your application has been deleted.",
-                            icon: "success"
-                        });
+                    .then(res => res.json())
+                    .then(data => {
+                        // console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your application has been deleted.",
+                                icon: "success"
+                            });
 
-                        const remAppliedVisas = myAppliedVisas.filter(myAppliedVisa => myAppliedVisa._id !== id);
-                        setMyAppliedVisa(remAppliedVisas);
-                    }else{
-                        Swal.fire({
-                            title: "Not Deleted!",
-                            text: "Your application has not been deleted.",
-                            icon: "error"
-                        });
-                    }
-                })
+                            const remAppliedVisas = myAppliedVisas.filter(myAppliedVisa => myAppliedVisa._id !== id);
+                            setMyAppliedVisa(remAppliedVisas);
+                        } else {
+                            Swal.fire({
+                                title: "Not Deleted!",
+                                text: "Your application has not been deleted.",
+                                icon: "error"
+                            });
+                        }
+                    })
             }
         });
     }
 
+    // handle search
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchingCountryName = e.target.searchingCountryName.value;
+        // console.log(searchingCountryName);
+
+        fetch(`${root_dir}/applications/${user.email}?searchingCountryName=${searchingCountryName}`)
+            .then(res => res.json())
+            .then(data => {
+                setMyAppliedVisa(data);
+                setLoading(false);
+            })
+    }
+
     return (
         <section className='max-w-8xl mx-auto px-5'>
-            {loading ? (<LoadingPage />) 
-            : myAppliedVisas.length > 0 ? (<div className='my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-                {
-                    myAppliedVisas.map(myAppliedVisa => <MyAppliedVisa
-                        key={myAppliedVisa._id}
-                        myAppliedVisa={myAppliedVisa}
-                        handleCancelVisa={handleCancelVisa}
-                    ></MyAppliedVisa>)
-                }
+            <div className="max-w-xs mx-auto mt-10">
+                <form onSubmit={handleSearch} className="flex flex-col sm:flex-row items-center gap-5">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input
+                            type="text"
+                            name="searchingCountryName"
+                            className="grow"
+                            placeholder="Search by country name"
+                        />
+                        <IoIosSearch className="text-2xl" />
+                    </label>
+                    <button type="submit" className="px-4 py-2 bg-[#003366] active:scale-95 rounded-lg text-white font-inter">Search</button>
+                </form>
+            </div>
+            {loading ? (<LoadingPage />)
+                : myAppliedVisas.length > 0 ? (<div className='my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                    {
+                        myAppliedVisas.map(myAppliedVisa => <MyAppliedVisa
+                            key={myAppliedVisa._id}
+                            myAppliedVisa={myAppliedVisa}
+                            handleCancelVisa={handleCancelVisa}
+                        ></MyAppliedVisa>)
+                    }
                 </div>) : (<NoDataFound />)}
         </section>
     );
