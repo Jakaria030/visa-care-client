e19d68708a606494815c25ec85118df506d3939b
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-08 12:15:05 +0600

    React Awesome reveal effect added

--- src/components/OurOfferings.jsx
+++ src/components/OurOfferings.jsx
@@ -2,41 +2,46 @@ import immigration from '../assets/immigration.png';
 import easyApplication from '../assets/comfort.png';
 import document from '../assets/passport.png';
 import interview from '../assets/interview.png';
+import { Zoom } from 'react-awesome-reveal';
 
 const OurOfferings = () => {
     return (
         <section className="max-w-8xl mx-auto px-5 my-10">
-            <div className="my-8 sm:my-10 flex items-center justify-center">
-                <h2 className="text-3xl font-poppins font-semibold"> <span className="text-[#003366]">What we</span> <span className="text-[#FF6F3F]">provide</span> </h2>
-            </div>
+            <Zoom>
+                <div className="my-8 sm:my-10 flex items-center justify-center">
+                    <h2 className="text-3xl font-poppins font-semibold"> <span className="text-[#003366]">What we</span> <span className="text-[#FF6F3F]">provide</span> </h2>
+                </div>
+            </Zoom>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
-                    <div className='size-32 mx-auto hover:scale-105'>
-                        <img className='w-full' src={immigration} />
+                <Zoom>
+                    <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
+                        <div className='size-32 mx-auto hover:scale-105'>
+                            <img className='w-full' src={immigration} />
+                        </div>
+                        <h3 className='text-xl font-poppins font-medium'> Responsibilities for Immigration Procedures </h3>
                     </div>
-                    <h3 className='text-xl font-poppins font-medium'> Responsibilities for Immigration Procedures </h3>
-                </div>
-                <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
-                    <div className='size-32 mx-auto hover:scale-105'>
-                        <img className='w-full' src={easyApplication} />
+                    <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
+                        <div className='size-32 mx-auto hover:scale-105'>
+                            <img className='w-full' src={easyApplication} />
+                        </div>
+                        <h3 className='text-xl font-poppins font-medium'> Simple & Fast Visa Application </h3>
                     </div>
-                    <h3 className='text-xl font-poppins font-medium'> Simple & Fast Visa Application </h3>
-                </div>
-                <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
-                    <div className='size-32 mx-auto hover:scale-105'>
-                        <img className='w-full' src={document} />
+                    <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
+                        <div className='size-32 mx-auto hover:scale-105'>
+                            <img className='w-full' src={document} />
+                        </div>
+                        <h3 className='text-xl font-poppins font-medium'> Required Documents
+                            Support </h3>
                     </div>
-                    <h3 className='text-xl font-poppins font-medium'> Required Documents
-                    Support </h3>
-                </div>
-                <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
-                    <div className='size-32 mx-auto hover:scale-105'>
-                        <img className='w-full' src={interview} />
+                    <div className='bg-gray-200 p-5 space-y-4 rounded-md hover:shadow-lg'>
+                        <div className='size-32 mx-auto hover:scale-105'>
+                            <img className='w-full' src={interview} />
+                        </div>
+                        <h3 className='text-xl font-poppins font-medium'> Skilled & Professional
+                            Consultants </h3>
                     </div>
-                    <h3 className='text-xl font-poppins font-medium'> Skilled & Professional
-                    Consultants </h3>
-                </div>
+                </Zoom>
             </div>
         </section>
     );
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,25 +1,28 @@
+import { Bounce } from "react-awesome-reveal";
 
 const Statistics = () => {
     return (
         <section className='bg-[#003366] text-white'>
-            <div className="max-w-8xl mx-auto px-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 py-10 my-10">
-                <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
-                    <h1 className="text-3xl font-bold font-poppins">100+</h1>
-                    <h3 className="text-lg font-inter">Visa Categories</h3>
+                <div className="max-w-8xl mx-auto px-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 py-10 my-10">
+            <Bounce cascade>
+                    <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
+                        <h1 className="text-3xl font-bold font-poppins">100+</h1>
+                        <h3 className="text-lg font-inter">Visa Categories</h3>
+                    </div>
+                    <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
+                        <h1 className="text-3xl font-bold font-poppins">98%</h1>
+                        <h3 className="text-lg font-inter">Satisfication Rate</h3>
+                    </div>
+                    <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
+                        <h1 className="text-3xl font-bold font-poppins">300+</h1>
+                        <h3 className="text-lg font-inter">Trusted Clients</h3>
+                    </div>
+                    <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
+                        <h1 className="text-3xl font-bold font-poppins">2.9k</h1>
+                        <h3 className="text-lg font-inter">Visa Process</h3>
+                    </div>
+            </Bounce>
                 </div>
-                <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
-                    <h1 className="text-3xl font-bold font-poppins">98%</h1>
-                    <h3 className="text-lg font-inter">Satisfication Rate</h3>
-                </div>
-                <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
-                    <h1 className="text-3xl font-bold font-poppins">300+</h1>
-                    <h3 className="text-lg font-inter">Trusted Clients</h3>
-                </div>
-                <div className='flex flex-col items-center justify-center py-5 sm:py-10 md:py-16 lg:py-20'>
-                    <h1 className="text-3xl font-bold font-poppins">2.9k</h1>
-                    <h3 className="text-lg font-inter">Visa Process</h3>
-                </div>
-            </div>
         </section>
     );
 };
