d7eccd94552fdd4ad4a746e64e0032140e362c91
Jakaria030 <gulamjakaria99@gmail.com>
2025-01-07 16:47:50 +0600

    Illustration image added in login and registration page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,8 @@ import { FaGoogle } from "react-icons/fa";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
+import login from "../assets/login.png";
+
 const LoginPage = () => {
 
     const { signInUser, setUser, signInWithGoogle, isDarkMode } = useContext(AuthContext);
@@ -51,38 +53,44 @@ const LoginPage = () => {
     return (
         <div className={`${isDarkMode ? 'bg-[#121212]' : 'bg-white'}`}>
             <section className='max-w-8xl mx-auto content-center px-5 min-h-[60vh] font-inter py-8 md:py-16'>
-                <div className='card w-full max-w-lg border-2 mx-auto py-5'>
-                    <h2 className={`text-2xl sm:text-3xl font-semibold font-poppins mx-4 sm:mx-8 text-center ${isDarkMode && 'text-[#F2F2F2]'}`}>Login Form</h2>
-                    <form onSubmit={handleLoginForm} className='card-body px-4 sm:px-8 pb-5'>
-                        <div className='form-control'>
-                            <label className='label'>
-                                <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Email</span>
-                            </label>
-                            <input type='email' name='email' placeholder='Email' className='input input-bordered' required />
-                        </div>
+                <div className="flex flex-col lg:flex-row border">
+                    <div className="basis-1/2 border-b lg:border-b-0 lg:border-r">
+                        <img className="max-h-[500px] object-center mx-auto" src={login} />
+                    </div>
 
-                        <div className='form-control'>
-                            <label className='label'>
-                                <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Password</span>
-                            </label>
-                            <input type='password' name='password' placeholder='Password' className='input input-bordered' required />
-                            <label className="label">
-                                <Link className={`label-text-alt link ${isDarkMode && 'text-[#F2F2F2]'}`}>Forgot password?</Link>
-                            </label>
-                            {
-                                logingError && <label className="label">
-                                    <p className="label-text text-[#D32F2F]">{logingError}</p>
+                    <div className='basis-1/2 card w-full max-w-lg mx-auto py-5'>
+                        <h2 className={`text-2xl sm:text-3xl font-semibold font-poppins mx-4 sm:mx-8 text-center ${isDarkMode && 'text-[#F2F2F2]'}`}>Login Form</h2>
+                        <form onSubmit={handleLoginForm} className='flex flex-col gap-2 px-4 sm:px-8 pb-5'>
+                            <div className='form-control'>
+                                <label className='label'>
+                                    <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Email</span>
                                 </label>
-                            }
-                        </div>
-                        <div className='form-control mt-5'>
-                            <button className={`py-2 rounded-md text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}>Login</button>
-                        </div>
-                    </form>
-                    <div className='form-control mx-4 sm:mx-8'>
-                        <button onClick={continueWithGoogle} className={`flex items-center gap-3 justify-center py-2 rounded-md bg-[#003366] text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}><FaGoogle /> <span>Continue with Google</span></button>
+                                <input type='email' name='email' placeholder='Email' className='input input-bordered' required />
+                            </div>
 
-                        <p className={`label-text text-center mt-3 ${isDarkMode && 'text-[#F2F2F2]'}`}>Don't have an account? <Link to='/registerPage' state={state} className='underline hover:text-[#FF6F3F] font-bold duration-150 transition-colors'>Register</Link></p>
+                            <div className='form-control'>
+                                <label className='label'>
+                                    <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Password</span>
+                                </label>
+                                <input type='password' name='password' placeholder='Password' className='input input-bordered' required />
+                                <label className="label">
+                                    <Link className={`label-text-alt link ${isDarkMode && 'text-[#F2F2F2]'}`}>Forgot password?</Link>
+                                </label>
+                                {
+                                    logingError && <label className="label">
+                                        <p className="label-text text-[#D32F2F]">{logingError}</p>
+                                    </label>
+                                }
+                            </div>
+                            <div className='form-control mt-5'>
+                                <button className={`py-2 rounded-md text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}>Login</button>
+                            </div>
+                        </form>
+                        <div className='form-control mx-4 sm:mx-8'>
+                            <button onClick={continueWithGoogle} className={`flex items-center gap-3 justify-center py-2 rounded-md bg-[#003366] text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}><FaGoogle /> <span>Continue with Google</span></button>
+
+                            <p className={`label-text text-center mt-3 ${isDarkMode && 'text-[#F2F2F2]'}`}>Don't have an account? <Link to='/registerPage' state={state} className='underline hover:text-[#FF6F3F] font-bold duration-150 transition-colors'>Register</Link></p>
+                        </div>
                     </div>
                 </div>
             </section>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,6 +3,8 @@ import { FaGoogle } from "react-icons/fa";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
+import signUp from '../assets/sign-up.png';
+
 const RegisterPage = () => {
 
     const { user, setUser, signUpUser, updateUserProfile, signInWithGoogle, isDarkMode } = useContext(AuthContext);
@@ -77,46 +79,52 @@ const RegisterPage = () => {
     return (
         <div className={`${isDarkMode ? 'bg-[#121212]' : 'bg-white'}`}>
             <section className='max-w-8xl mx-auto px-5 font-inter py-8 md:py-16'>
-                <div className='card w-full max-w-lg border-2 mx-auto py-5'>
-                    <h2 className={`text-2xl sm:text-3xl font-semibold mx-4 sm:mx-8 font-poppins text-center ${isDarkMode && 'text-[#F2F2F2]'}`}>Registration Form</h2>
-                    <form onSubmit={handleRegisterForm} className='card-body px-4 sm:px-8 py-5'>
-                        <div className='form-control'>
-                            <label className='label'>
-                                <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Name</span>
-                            </label>
-                            <input type='text' name='name' placeholder='Name' className='input input-bordered' required />
-                        </div>
-                        <div className='form-control'>
-                            <label className='label'>
-                                <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Email</span>
-                            </label>
-                            <input type='email' name='email' placeholder='Email' className='input input-bordered' required />
-                        </div>
-                        <div className='form-control'>
-                            <label className='label'>
-                                <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Photo URL</span>
-                            </label>
-                            <input type='text' name='photoURL' placeholder='Photo URL' className='input input-bordered' required />
-                        </div>
-                        <div className='form-control'>
-                            <label className='label'>
-                                <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Password</span>
-                            </label>
-                            <input type='password' name='password' placeholder='Password' className='input input-bordered' required />
-                            {
-                                registerError && <label className="label">
-                                    <p className="label-text text-[#D32F2F]">{registerError}</p>
-                                </label>
-                            }
-                        </div>
-                        <div className='form-control mt-5'>
-                            <button className={`py-2 rounded-md text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}>Register</button>
-                        </div>
-                    </form>
-                    <div className='form-control mx-4 sm:mx-8'>
-                        <button onClick={continueWithGoogle} className={`flex items-center gap-3 justify-center py-2 rounded-md text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}><FaGoogle /> <span>Continue with Google</span></button>
+                <div className="flex flex-col lg:flex-row border">
+                    <div className="basis-1/2 border-b lg:border-b-0 lg:border-r p-5">
+                        <img className="max-h-[600px] mx-auto object-center" src={signUp} />
+                    </div>
 
-                        <p className={`label-text text-center mt-3 ${isDarkMode && 'text-[#F2F2F2]'}`}>Already have an account? <Link to='/loginPage' className='underline hover:text-[#FF6F3F] font-bold duration-150 transition-colors'>Login</Link></p>
+                    <div className='basis-1/2 card w-full max-w-lg mx-auto py-5'>
+                        <h2 className={`text-2xl sm:text-3xl font-semibold mx-4 sm:mx-8 mb-5 font-poppins text-center ${isDarkMode && 'text-[#F2F2F2]'}`}>Registration Form</h2>
+                        <form onSubmit={handleRegisterForm} className='flex flex-col gap-2 px-4 sm:px-8'>
+                            <div className='form-control'>
+                                <label className='label'>
+                                    <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Name</span>
+                                </label>
+                                <input type='text' name='name' placeholder='Name' className='input input-bordered' required />
+                            </div>
+                            <div className='form-control'>
+                                <label className='label'>
+                                    <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Email</span>
+                                </label>
+                                <input type='email' name='email' placeholder='Email' className='input input-bordered' required />
+                            </div>
+                            <div className='form-control'>
+                                <label className='label'>
+                                    <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Photo URL</span>
+                                </label>
+                                <input type='text' name='photoURL' placeholder='Photo URL' className='input input-bordered' required />
+                            </div>
+                            <div className='form-control'>
+                                <label className='label'>
+                                    <span className={`label-text ${isDarkMode && 'text-[#F2F2F2]'}`}>Password</span>
+                                </label>
+                                <input type='password' name='password' placeholder='Password' className='input input-bordered' required />
+                                {
+                                    registerError && <label className="label">
+                                        <p className="label-text text-[#D32F2F]">{registerError}</p>
+                                    </label>
+                                }
+                            </div>
+                            <div className='form-control mt-5'>
+                                <button className={`py-2 rounded-md text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}>Register</button>
+                            </div>
+                        </form>
+                        <div className='form-control mx-4 sm:mx-8 mt-5'>
+                            <button onClick={continueWithGoogle} className={`flex items-center gap-3 justify-center py-2 rounded-md text-[#F2F2F2] active:scale-95 ${isDarkMode ? 'bg-[#FF6F3F]' : 'bg-[#003366]'}`}><FaGoogle /> <span>Continue with Google</span></button>
+
+                            <p className={`label-text text-center mt-3 ${isDarkMode && 'text-[#F2F2F2]'}`}>Already have an account? <Link to='/loginPage' className='underline hover:text-[#FF6F3F] font-bold duration-150 transition-colors'>Login</Link></p>
+                        </div>
                     </div>
                 </div>
             </section>
